46f4405eebd2d9cebdd4898dad119d41ef656c7c
Dany Mapathi <danymapathi@gmail.com>
2023-01-29 12:58:48 +0100

    update db credentials

--- dbConnector.php
+++ dbConnector.php
@@ -10,8 +10,8 @@ var $link;
   // Get the main settings from the array we just loaded
         $host = 'localhost';
         $db = 'capstone';
-        $user = 'root';
-        $pass = '';
+        $user = 'capstone';
+        $pass = 'capstone';
 
         // Connect to the database
         $this->link = mysqli_connect($host, $user, $pass,$db);
